b3e552cc45da2a839cde8ad9ccdb325fa50d8eb6
kkkwwwmmm <127164759+kkkwwwmmm@users.noreply.github.com>
2024-04-23 11:38:45 +0900

    ?????

--- Midterm/Assets/Script/move.cs
+++ Midterm/Assets/Script/move.cs
@@ -13,20 +13,26 @@ public class PlayerMovement : MonoBehaviour
         horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
 
-        // 현재 카메라의 전방 방향으로 이동 방향 설정
-        moveDirection = transform.forward * verticalInput + transform.right * horizontalInput;
-        moveDirection.Normalize(); // 정규화하여 이동 방향 벡터를 단위 벡터로 만듦
-
-        // O 키를 눌렀을 때 y축으로 90도 회전
+        // O 키를 눌렀을 때 회전
         if (Input.GetKeyDown(KeyCode.O))
+        {
+            transform.Rotate(Vector3.up, -90f);
+        }
+         if (Input.GetKeyDown(KeyCode.P))
         {
             transform.Rotate(Vector3.up, 90f);
         }
+
+        // 현재 카메라의 전방 방향으로 이동 방향 설정
+        moveDirection = transform.forward * verticalInput + transform.right * horizontalInput;
+        moveDirection.Normalize(); // 정규화하여 이동 방향 벡터를 단위 벡터로 만듦
 
         // 이동 적용
         transform.position += moveDirection * moveSpeed * Time.deltaTime;
     }
 }
+
+
 
 
 
